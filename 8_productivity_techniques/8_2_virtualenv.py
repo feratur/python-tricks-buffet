64572540d786ff633b4add7b099fece4b8b48876
Alexey Burlakov <feraturdev@gmail.com>
2021-08-24 22:35:21 +0300

    Add Chapter 8.2

---
+++ 8_productivity_techniques/8_2_virtualenv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+if __name__ == '__main__':
+    # A virtual environment is an isolated Python environment. Physically,
+    # it lives inside a folder containing all the packages and other dependencies,
+    # like native-code libraries and the interpreter runtime, that
+    # a Python project needs.
+    #
+    # which pip3
+    # > /usr/local/bin/pip3
+    #
+    # python3 -m venv ./venv
+    # source ./venv/bin/activate
+    # which pip3
+    # > /Users/dan/my-project/venv/bin/pip3
+    # which python
+    # > /Users/dan/my-project/venv/bin/python
+    # deactivate
+    # which pip3
+    # > /usr/local/bin/pip3
+
+    # Virtual environments keep your project dependencies separated.
+    # They help you avoid version conflicts between packages
+    # and different versions of the Python runtime.
+    #
+    # As a best practice, all of your Python projects should use virtual
+    # environments to store their dependencies. This will help avoid
+    # headaches.
